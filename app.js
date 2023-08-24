8761b9e3bdf4d9fc15ea85b7dc5dd004bff3537f
tajkhan-dev <bbadshah691@gmail.com>
2023-08-24 10:02:25 +0500

    project is set

---
+++ app.js
@@ -0,0 +1,3 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log('this is the first console');
---
+++ app.ts
@@ -0,0 +1,14 @@
+
+
+
+import inquirer from 'inquirer'
+
+console.log('this is the first console')
+
+const Counter=async ()=>{
+    const {word}=await inquirer.prompt([
+        {
+
+        }
+    ])
+}
